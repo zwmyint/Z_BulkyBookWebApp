0e2afa1fb8208808de2fc3419974c664aa27ce07
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2021-07-18 17:06:26 +0800

    Add project files.

---
+++ Z_BulkyBookWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Z_BulkyBookWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
